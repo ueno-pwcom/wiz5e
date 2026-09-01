c825132570de5f18e44906d9ae7cea2eb8f8bc83
ueno <ueno@pwcom.co.jp>
2026-09-01 09:58:29 +0900

    エンカウント率修正

--- src/data/map1.ts
+++ src/data/map1.ts
@@ -160,7 +160,7 @@ export const map1Data: DungeonMap = {
     facing: 'N'
   },
   encounter_table: {
-    rate: 0.5,
+    rate: 0.1,
     monsters: [
       { id: 'zombie', weight: 10 },
       { id: 'kobold', weight: 50 },
